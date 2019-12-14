ee4d068c77f8904f8bbf233d7e854bb2f7fb2c0f
rick <rick@jenkins-zh.cn>
2019-12-14 17:53:40 +0800

    Add start and limit option for the list API

--- src/main/java/io/jenkins/plugins/pipeline/restful/api/ItemAPI.java
+++ src/main/java/io/jenkins/plugins/pipeline/restful/api/ItemAPI.java
@@ -38,15 +38,21 @@ public class ItemAPI implements RootAction {
 
     @ServeJson
     public List<SimpleItem> doList(@QueryParameter String name,
-                                   @QueryParameter String type) {
+                                   @QueryParameter String type,
+                                   @QueryParameter int start,
+                                   @QueryParameter int limit) {
         List<Item> items = Jenkins.get().getAllItems();
         List<SimpleItem> simpleItems = new ArrayList<>();
 
         boolean filterName = StringUtils.isNotEmpty(name);
         boolean filterType = StringUtils.isNotEmpty(type);
 
+        start = start < 0 ? 0 : start;
+        limit = limit <= 0 ? 50 : limit;
+
         items.stream().filter(item -> !filterName || item.getName().contains(name)).
                 filter(item -> !filterType || item.getClass().getSimpleName().contains(type)).
+                skip(start).limit(limit).
                 forEach(item -> simpleItems.add(SimpleItemUtils.convert(item)));
 
         return simpleItems;
