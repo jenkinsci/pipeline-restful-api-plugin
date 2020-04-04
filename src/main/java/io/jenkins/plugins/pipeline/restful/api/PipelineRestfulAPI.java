c2df821e24e6d4545a0f07404cb216e61d5c54c5
Sladyn Nunes <sladynnunes98@gmail.com>
2020-04-04 12:14:03 +0530

    Add pluginList endpoint

--- src/main/java/io/jenkins/plugins/pipeline/restful/api/PipelineRestfulAPI.java
+++ src/main/java/io/jenkins/plugins/pipeline/restful/api/PipelineRestfulAPI.java
@@ -3,11 +3,17 @@ package io.jenkins.plugins.pipeline.restful.api;
 import com.cloudbees.workflow.rest.AbstractWorkflowJobActionHandler;
 import com.cloudbees.workflow.util.ModelUtil;
 import com.cloudbees.workflow.util.ServeJson;
+import com.fasterxml.jackson.databind.JsonNode;
 import hudson.Extension;
 import hudson.ExtensionList;
+import hudson.Plugin;
+import hudson.PluginWrapper;
 import hudson.model.Item;
 import hudson.util.HttpResponses;
+import java.net.HttpURLConnection;
+import java.util.List;
 import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.flow.FlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
@@ -18,6 +24,7 @@ import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import java.io.IOException;
+import org.kohsuke.stapler.verb.GET;
 
 @Extension
 @Restricted(NoExternalUse.class)
@@ -58,5 +65,17 @@ public class PipelineRestfulAPI extends AbstractWorkflowJobActionHandler {
         job.save();
 
         return HttpResponses.ok();
+    }
+
+    @ServeJson
+    public JSONObject doGetPluginList() throws IOException {
+        List<PluginWrapper> pluginList = Jenkins.get().pluginManager.getPlugins();
+        JSONObject pluginJSON = new JSONObject();
+        for(PluginWrapper pluginWrapper: pluginList) {
+            pluginJSON.put("artifactId", pluginWrapper.getDisplayName());
+            pluginJSON.put("groupId", pluginWrapper.getLongName());
+            pluginJSON.put("source", new JSONObject().put("version", pluginWrapper.getVersion()));
+        }
+        return pluginJSON;
     }
 }
